5a6e2c6ffc629a50c646ffaa6ef1d410b1a07012
Ivo Yovchev <ivo@tornado-studios.com>
2025-04-17 20:13:39 +0200

    Initial Commit

---
+++ climbingtracker99Tests/climbingtracker99Tests.swift
@@ -0,0 +1,17 @@
+//
+//  climbingtracker99Tests.swift
+//  climbingtracker99Tests
+//
+//  Created by Ivo Yovchev on 17/04/2025.
+//
+
+import Testing
+@testable import climbingtracker99
+
+struct climbingtracker99Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
