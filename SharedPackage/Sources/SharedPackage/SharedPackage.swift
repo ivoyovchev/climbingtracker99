a4814746c92282d04e875605cf44e99754dc3509
Ivo Yovchev <ivo@tornado-studios.com>
2025-04-18 15:37:26 +0200

    v1.0.1: Update version number in Settings and README

---
+++ SharedPackage/Sources/SharedPackage/SharedPackage.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public enum SharedConstants {
+    public static let APP_VERSION = "1.0.1"
+    public static let APP_GROUP_IDENTIFIER = "group.com.tornado-studios.climbingtracker99"
+}
